5ba397770562fca3d51b2f4133cc33d668808ded
leiliang <1007279149@qq.com>
2016-01-14 16:33:02 +0800

    Initial Commit

---
+++ HMChart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HMChart
+//
+//  Created by mac on 16/1/14.
+//  Copyright © 2016年 leiliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HMChart/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HMChart
+//
+//  Created by mac on 16/1/14.
+//  Copyright © 2016年 leiliang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
